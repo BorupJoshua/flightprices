ac1e24a27afafa08796fc4186efdb7e5aafa5a1b
Joshua Borup <jb458@mst.edu>
2021-05-08 01:52:38 -0500

    updated main.py

--- main.py
+++ main.py
@@ -2,45 +2,70 @@ from time import sleep, strftime
 from random import randint
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+from datetime import timedelta
+from datetime import date
 import smtplib
 
 
 # Configuration Options
 
+# Number of days to look in the future
+# Recommended 293 since the main carriers don't post their prices atleast 330 days in advance, then give 30 days for the month view and an additional 7 days as a buffer.
 days_to_look_ahead = 293
 
+# The specific class on the website that represents the lowest fare
 lowest_price_class_string = 'und0'
 
+# Chromedriver's path
 chromedriver_path = 'chromedriver.exe'
 
-
-
+# Kayak's url stuff, the first should not change, but the 2nd (kayak_closer) should be copied after the 2nd date in the url.
+# TO DO: Create a system to automatically know number of travelers (in my case it's 8)
 kayak_before_destination = 'https://www.kayak.com/flights/'
 kayak_closer = '-flexible-calendar-10to14/8adults?sort=bestflight_a&fs=cfc=0;bfc=0'
 
 
+# Actual page scraping function
+# INPUT: Two string IATA Codes
+# OUTPUT: VOID
 def page_scrape(iataFROM, iataTO):
 
-    date_start = '2022-03-01'
-    date_end = '2022-03-30' 
+    # Get the date object of today + days to look at (293 is default)
+    future_date = date.today() + timedelta(days=days_to_look_ahead)
 
+    # Add an additional 29 days to mark the end point
+    future_date_plus_month = future_date + timedelta(days=29)
+
+    # Convert the date time objects into strings
+    date_start = future_date.strftime("%Y-%m-%d")
+    date_end = future_date_plus_month = future_date.strftime("%Y-%m-%d")
+
+    # Create the URL we're gonna be looking at, the url that doesn't change and the dates and iata codes
     url = ''+kayak_before_destination+iataFROM+'-'+iataTO+'/'+date_start+'/'+date_end+kayak_closer
 
-    
+    # Create the webdriver and start the chromdriver exe
     driver = webdriver.Chrome(executable_path=chromedriver_path)
+
+    # Open the webpage
     driver.get(url)
 
-    sleep(10)
+    # Wait for the page to fully populate the results
+    sleep(50)
 
+    # Find the element that has the specific class that represents the lowest price, then grab the price value
     day_container = driver.find_element_by_class_name(lowest_price_class_string)
     price_container = day_container.find_element_by_class_name('price')
     price = price_container.text
 
+    # Filter out non numeric numbers from the container text
     numeric_filter = filter(str.isdigit, price)
     price_cleaned = "".join(numeric_filter)
 
+    # For now, print out the price
     print(price_cleaned)
     
+    # Close the driver as we're done here
     driver.close()
 
 
@@ -49,5 +74,9 @@ def page_scrape(iataFROM, iataTO):
 sleep(2)
 
 page_scrape('SGF','NRT')
+
+sleep(2)
+
+page_scrape('COU','NRT')
 
 
