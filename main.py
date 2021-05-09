3d798c2ff659ebe413b8ffd49fc4e5a9718697a1
Joshua Borup <jb458@mst.edu>
2021-05-09 14:31:10 -0500

    updated main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from time import sleep, strftime
-from random import randint
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from datetime import timedelta
 from datetime import date
-import pandas as pd
-import smtplib
+from datetime import datetime
 import csv
 import os.path
 import requests
 from discord import Webhook, RequestsWebhookAdapter
+import discord
+import operator
 
 
 
@@ -54,7 +54,6 @@ csv_file_name = 'historical_data.csv'
 webhook_file_path = 'webhook.txt'
 
 # Kayak's url stuff, the first should not change, but the 2nd (kayak_closer) should be copied after the 2nd date in the url.
-# TO DO: Create a system to automatically know number of travelers (in my case it's 8)
 kayak_before_destination = 'https://www.kayak.com/flights/'
 kayak_closer = '-flexible-calendar-10to14/'+num_adults+'adults?sort=bestflight_a&fs=cfc=0;bfc=0'
 
@@ -107,10 +106,13 @@ def page_scrape(iataFROM, iataTO):
 
     # return the price
     return price_cleaned
+#end page_scrape
 
 
 # Load Stack from CSV if exists
 # Create Empty Stack if it doesnt
+# Input: Nothing
+# Output: queue of tuples(int, string)
 def load_stack():
     if not (os.path.exists(csv_file_name)):
         open(csv_file_name, 'a+')
@@ -124,12 +126,15 @@ def load_stack():
         for row in csv_reader:
             if len(row) == 0:
                 continue
+            row[0] = int(row[0])
             data.append(tuple(row))
 
     # Return the stack of tuples
     return data
+# End load_stack
 
 # Saves the stack
+# Input: queue of tuples(int, string)
 def save_stack(data):
 
     # Opens the file for writing
@@ -139,21 +144,28 @@ def save_stack(data):
         csv_out = csv.writer(file)
         for row in data:
             csv_out.writerow(row)
+# End save_Stack
         
-
+# Pops the top of the stack until we get the timeframe of data we want
+# Input: Integer, queue of tuples(int,string)
+# Output: queue of tuples(int, string)
 def clean_stack(timeframe, data):
     
-    while (data.size() > (timeframe * number_of_iterations)):
+    while (len(data) > (timeframe * number_of_iterations)):
         data.pop(0)
 
     return data
+# End Clean Stack
 
+# Returns the lowest tuple in the timeframe provided
+# Input: Integer, queue of tuples(int, string)
+# Output: Integer
 def return_lowest_in_timeframe(timeframe, dataset):
 
-    new_dataset = clean_stack(timeframe,data)
+    new_dataset = clean_stack(timeframe,dataset)
 
-    return min(data, key = lambda t; t[0])
-
+    return min(dataset, key=operator.itemgetter(0))
+# End return_lowest_in_timeframe
 
 #Main Driver Function
 def main():
@@ -180,6 +192,7 @@ def main():
     
     # Create a new tuple of the lowest price and the flight pair string
     lowest_tuple_today = (lowest_price,flight_pair)
+    lowest_today_str = str(lowest_tuple_today[0])+" ("+lowest_tuple_today[1]+")"
 
     # Add it to the data stack
     data.append(lowest_tuple_today)
@@ -192,23 +205,39 @@ def main():
 
     # Grab the lowest price overall
     lowest_tuple_overall = return_lowest_in_timeframe(days_to_keep_data,data)
+    lowest_overall_str = str(lowest_tuple_overall[0])+" ("+lowest_tuple_overall[1]+")"
 
     # Grab the lowest monthly
     lowest_tuple_monthly = return_lowest_in_timeframe(30,data)
+    lowest_monthly_str = str(lowest_tuple_monthly[0])+" ("+lowest_tuple_monthly[1]+")"
 
     # Grab the lowest weekly
     lowest_tuple_weekly = return_lowest_in_timeframe(7,data)
-=
+    lowest_weekly_str = str(lowest_tuple_weekly[0])+" ("+lowest_tuple_weekly[1]+")"
 
     # Discord integration
+
+    # Open webhook file (secret!)
     webhook_secret_file = open(webhook_file_path, 'r')
 
+    # Grab the url in the file
     webhook_secret = webhook_secret_file.read()
 
-    message = "LOWEST PRICE CURRENTLY: $"+str(lowest_price)+", AIRPORT PAIR: "+flight_pair
-
+    # Create the webhook from the url
     webhook = Webhook.from_url(webhook_secret, adapter=RequestsWebhookAdapter())
-    webhook.send(message)
+
+    # Create the embeded object to send to discord
+    embed=discord.Embed(title="Flight Prices Update", color=0xff0033)
+    embed.set_author(name="FLIGHT WATCH DOG", url="https://matrix.itasoftware.com", icon_url="https://w7.pngwing.com/pngs/205/97/png-transparent-airplane-icon-a5-takeoff-computer-icons-flight-airplane.png")
+    embed.add_field(name="Lowest Price Today", value=lowest_today_str, inline=False)
+    embed.add_field(name="Lowest Price Weekly", value=lowest_weekly_str, inline=True)
+    embed.add_field(name="Lowest Price Monthly", value=lowest_monthly_str, inline=True)
+    embed.add_field(name="Lowest Price Overall", value=lowest_overall_str, inline=True)
+    embed.set_footer(text="Updated: "+datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+    webhook.send(embed=embed)
+
+
+# End Main
 
 
 
